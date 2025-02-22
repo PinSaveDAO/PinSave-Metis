ab5149a445da6657f0baff1745d27352b15ccfbf
name <fedorovn19@gmail.com>
2025-02-22 05:18:11 +0300

    fixing seo

--- packages/hardhat/hardhat.config.js
+++ packages/hardhat/hardhat.config.js
@@ -29,53 +29,53 @@ module.exports = {
   networks: {
     mumbai: {
       url: `https://polygon-mumbai.g.alchemy.com/v2/${process.env.ALCHEMY_API_KEY}`,
-      accounts: [process.env.PRIVATE_KEY, process.env.PRIVATE_KEY_2],
+      accounts: [process.env.PRIVATE_KEY],
       chainId: 80001,
       gas: 8000000,
       gasPrice: 1000000000,
     },
     l16: {
       url: "https://rpc.l16.lukso.network",
-      accounts: [process.env.PRIVATE_KEY, process.env.PRIVATE_KEY_2],
+      accounts: [process.env.PRIVATE_KEY],
       chainId: 2828,
       gas: 500000000000000,
       gasPrice: 300000000000,
     },
     l14: {
       url: "https://rpc.l14.lukso.network",
-      accounts: [process.env.PRIVATE_KEY, process.env.PRIVATE_KEY_2],
+      accounts: [process.env.PRIVATE_KEY],
       chainId: 22,
       gas: 50000000,
       gasPrice: 1000000000000,
     },
     evmos: {
       url: "https://eth.bd.evmos.dev:8545",
-      accounts: [process.env.PRIVATE_KEY, process.env.PRIVATE_KEY_2],
+      accounts: [process.env.PRIVATE_KEY],
       chainId: 9000,
     },
     fantom: {
       url: "https://rpc.ankr.com/fantom/",
-      accounts: [process.env.PRIVATE_KEY, process.env.PRIVATE_KEY_2],
+      accounts: [process.env.PRIVATE_KEY],
       chainId: 250,
     },
     bsc: {
       url: "https://bscrpc.com",
-      accounts: [process.env.PRIVATE_KEY, process.env.PRIVATE_KEY_2],
+      accounts: [process.env.PRIVATE_KEY],
       chainId: 56,
     },
     metis: {
       url: "https://andromeda.metis.io",
-      accounts: [process.env.PRIVATE_KEY, process.env.PRIVATE_KEY_2],
+      accounts: [process.env.PRIVATE_KEY],
       chainId: 1088,
     },
     mantle: {
       url: "https://rpc.testnet.mantle.xyz/",
-      accounts: [process.env.PRIVATE_KEY, process.env.PRIVATE_KEY_2],
+      accounts: [process.env.PRIVATE_KEY],
       chainId: 5001,
     },
     filecoin: {
       url: "https://rpc.ankr.com/filecoin",
-      accounts: [process.env.PRIVATE_KEY, process.env.PRIVATE_KEY_2],
+      accounts: [process.env.PRIVATE_KEY],
       chainId: 314,
     },
     optimism: {
